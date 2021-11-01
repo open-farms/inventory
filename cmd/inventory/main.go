a861da971cf4c0d1b23ec9bc5378db6eaaf99e9e
Britton Hayes <brittonhayes@users.noreply.github.com>
2021-11-01 00:00:05 -0700

    chore: initial commit

---
+++ cmd/inventory/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"os"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/open-farms/inventory/internal/vehicle"
+
+	vehicleService "github.com/open-farms/inventory/api/vehicles/service/v1"
+)
+
+// go build -ldflags "-X main.Version=x.y.z"
+var (
+	// Name is the name of the compiled software.
+	Name string
+	// Version is the version of the compiled software.
+	Version string
+
+	id, _ = os.Hostname()
+)
+
+func main() {
+
+	logger := log.With(log.NewStdLogger(os.Stdout),
+		"ts", log.DefaultTimestamp,
+		"caller", log.DefaultCaller,
+		"service.id", id,
+		"service.name", Name,
+		"service.version", Version,
+		"trace_id", tracing.TraceID(),
+		"span_id", tracing.SpanID(),
+	)
+
+	httpSrv := http.NewServer(
+		http.Address(":8000"),
+		http.Middleware(
+			recovery.Recovery(),
+		),
+	)
+
+	grpcSrv := grpc.NewServer(
+		grpc.Address(":9000"),
+		grpc.Middleware(
+			recovery.Recovery(),
+		),
+	)
+
+	svc := vehicle.NewVehicleService()
+	vehicleService.RegisterVehicleServiceServer(grpcSrv, svc)
+	vehicleService.RegisterVehicleServiceHTTPServer(httpSrv, svc)
+
+	app := kratos.New(
+		kratos.ID(id),
+		kratos.Name(Name),
+		kratos.Version(Version),
+		kratos.Metadata(map[string]string{}),
+		kratos.Logger(logger),
+		kratos.Server(
+			httpSrv,
+			grpcSrv,
+		),
+	)
+
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/open-farms/inventory
+
+go 1.16
+
+require (
+	github.com/AlecAivazis/survey/v2 v2.3.2 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/go-kratos/kratos/cmd/kratos/v2 v2.0.0-20211028030931-143dc584cb8a // indirect
+	github.com/go-kratos/kratos/cmd/protoc-gen-go-errors/v2 v2.0.0-20211028030931-143dc584cb8a // indirect
+	github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2 v2.0.0-20211028030931-143dc584cb8a // indirect
+	github.com/go-kratos/kratos/v2 v2.1.1
+	github.com/google/wire v0.5.0
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/sys v0.0.0-20211031064116-611d5d643895 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20211029142109-e255c875f7c7
+	google.golang.org/grpc v1.40.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.27.1
+)
